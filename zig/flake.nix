483e30eace4012641d7a87ba02d1c016983fca78
ifelse023 <ifelse023@gmail.com>
2024-07-08 06:10:27 +0200

    zig

--- zig/flake.nix
+++ zig/flake.nix
@@ -1,20 +1,34 @@
 {
-  description = "A Nix-flake-based Zig development environment";
+  description = "Zig development environment";
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nativeBuildInputs = with pkgs; [
+          zig
+          zls
+        ];
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [ zig ];
+        buildInputs = with pkgs; [ ];
+      in
+      {
+        devShells.default = pkgs.mkShell { inherit nativeBuildInputs buildInputs; };
+
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "name";
+          version = "0.0.0";
+          src = ./.;
+
+          nativeBuildInputs = nativeBuildInputs ++ [ pkgs.zig.hook ];
+          inherit buildInputs;
         };
-      });
-    };
+      }
+    );
 }
